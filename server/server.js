dc26cd9f79de79918fdf5592034b6ca2e2f9bb7a
theyaintjay <133445701+theyaintjay@users.noreply.github.com>
2024-01-23 10:54:36 -0600

    installed node.js and npm; installed stripe

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+const storeItems = new Map([
+  [1, {priceInCents: 10000, name: "Learn React today"}],
+  [2, {priceInCents: 20000, name:"Learn CSS today"}],
+]);
+
+app.listen(3000);
