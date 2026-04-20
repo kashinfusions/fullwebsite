488f67aade5d61db3a4482b83b749d555d77d813
SylKyla Cole <sylkylacole@gmail.com>
2026-04-20 14:57:15 -0500

    Small changes to backend code

--- kashinfusions-backend/backend.js
+++ kashinfusions-backend/backend.js
@@ -1,1 +1,11 @@
 console.log("Backend file connected");
+
+// import html elements that will be altered
+// (on candle page) price, each select opiton, the add to cart button, the plus and minus quantity selectors, & the number between them
+// (cart side menu) number in blue bubble, div for products, the total number on the right, & the checkout button (redirects to stripe)
+// (on every page) cart number at the top
+
+// write function to implement the price (write after importing products)
+// need to pull price from each specific product into the js and assign variables to them
+// strong[class="text-primary display-6"] -- translate into js OR pull a class only
+ const priceHeader = document.getElementsByClassName("text-primary display-6"); 
