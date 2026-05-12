c248eb49b2f519106c0834244a654fd21d363d0e
SylKyla Cole <sylkylacole@gmail.com>
2026-05-11 20:12:11 -0500

    Minor JS changes when trying to import select statement; very barebones

--- kashinfusions-backend/backend.js
+++ kashinfusions-backend/backend.js
@@ -1,11 +1,30 @@
 console.log("Backend file connected");
 
 // import html elements that will be altered
+const sizeSelection = document.getElementById("size-select-dropdown");
+const sizeOptionOne = document.getElementById("select-option-1");
+
 // (on candle page) price, each select opiton, the add to cart button, the plus and minus quantity selectors, & the number between them
+
+
 // (cart side menu) number in blue bubble, div for products, the total number on the right, & the checkout button (redirects to stripe)
+
+
 // (on every page) cart number at the top
 
-// write function to implement the price (write after importing products)
+
 // need to pull price from each specific product into the js and assign variables to them
+
 // strong[class="text-primary display-6"] -- translate into js OR pull a class only
  const priceHeader = document.getElementsByClassName("text-primary display-6"); 
+
+ // write function to implement the price (write after importing products)
+
+
+// var sizeValue = sizeSelection.value;
+// var sizeText = sizeSelection.selectedIndex.text;
+
+console.log(sizeSelection);
+console.log(sizeOptionOne);
+// console.log(sizeValue);
+// console.log(sizeText);
